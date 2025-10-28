4db383b3631bfedd5e39218c401dea264475e30a
tsukishikawa <mariishikawamota@hotmail.com>
2025-10-28 10:24:44 -0300

    Controller

---
+++ src/main/java/com.delivery_api/Projeto.Delivery.API/controller/HealthController.java
@@ -0,0 +1,22 @@
+package com.delivery_api.Projeto.Delivery.API.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public Map<String, String>health(){
+        return Map.of(
+                "status", "UP",
+                "timestamp", LocalDateTime.now().toString(),
+                "Service", "Delivery API",
+                "message", "OK"
+        );
+    }
+}
